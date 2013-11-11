87e1fef558c266356d3638d024f1cb14dd9bde4e
Carolina <carolinanobre@gmail.com>
2013-11-11 16:02:00 -0500

    changes to config file

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'carolinanobre@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p2_carolinanobre_biz'); 
+	define('DB_NAME', 'caroli11_p2_carolinanobre_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
